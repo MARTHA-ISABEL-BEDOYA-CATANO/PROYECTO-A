cb5d4a61440e281f1f72bb7660f879dffce25d18
luiscarlos27 <lszondas@gmail.com>
2022-10-01 20:22:37 -0500

    Conexion de la base de datos

---
+++ FUNDACAQUETA/src/Models/Conexion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author clockwork
+ */
+public class Conexion {
+    
+    private String usuario = "servidor";
+    private String url = "jdbc:mysql://localhost:3306/FUNDACAQUETA";  
+    private String Contraseña ="";
+    
+    public Connection Conectar(){
+        Connection link = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            link = DriverManager.getConnection(this.url, this.usuario, this.Contraseña);
+            JOptionPane.showMessageDialog(null, "Conexion abierta");
+            
+                      
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.toString());    
+           
+        }
+        
+        return link;
+    }
+    
+}
