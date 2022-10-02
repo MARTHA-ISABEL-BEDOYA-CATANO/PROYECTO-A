fc925090639015d56f1637834ff1b7632cbeb8f1
luiscarlos27 <lszondas@gmail.com>
2022-10-01 20:27:29 -0500

    Elementos necesarios

--- FUNDACAQUETA/src/fundacaqueta/FUNDACAQUETA.java
+++ FUNDACAQUETA/src/fundacaqueta/FUNDACAQUETA.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package fundacaqueta;
-
+import Models.Conexion;
 /**
  *
  * @author clockwork
@@ -16,6 +16,11 @@ public class FUNDACAQUETA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Conexion exe = new Conexion();
+        exe.Conectar();
+        
+        
+        
     }
     
 }
